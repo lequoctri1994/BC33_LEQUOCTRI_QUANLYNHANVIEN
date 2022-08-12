f5df6bc65bbd98eadcf2c75b7fbaf018bc0cbe6e
lequoctri1994 <lequoctri1994@gmail.com>
2022-08-12 23:34:16 +0700

    asd

--- js/index.js
+++ js/index.js
@@ -23,6 +23,9 @@ function createStaff() {
   var chucVu = document.querySelector("#chucvu").value;
   var gioLam = +document.querySelector("#gioLam").value;
 
+  var valid = true;
+valid = kiemTraRong(createStaff.tenNV,'#name','Tên Nhân Viên');
+
   var staff = new Staff(
     taiKhoanNV,
     tenNV,
@@ -45,14 +48,13 @@ function renderStaffList (arrStaff) {
     for (var i = 0; i < arrStaff.length; i++){
         var obStaff = arrStaff[i];
         obStaff.tongLuong = function () {
-          var chucVu = document.querySelector('#chucvu').value;
-          var luong = this.luongCB * this.gioLam;
-          if (chucVu == "Sếp") {
-            return luong * 3;
-          } else if (chucVu == "Trưởng phòng") {
-            return luong * 2;
+          // var chucVu = document.querySelector('#chucvu').value;
+          if (arrStaff[i].chucVu == "Sếp") {
+            return this.luongCB * 3;
+          } else if (arrStaff[i].chucVu == "Trưởng phòng") {
+            return this.luongCB * 2;
           } else {
-            return luong;
+            return this.luongCB;
           }
         };
 
@@ -156,21 +158,24 @@ function UpdateStaff () {
   }
 }
 
+
 function searchStaff() {
   var keyWord = document.querySelector('#searchName').value;
   
   var res = [];
 
   for (i = 0; i < staffList.length; i++) {
-    if(staffList[i].tenNV.search(keyWord) != -1) {
+      var tenNV = removeVietnameseTones(staffList[i].tenNV);
+    if(tenNV.search(keyWord) != -1) {
+
       res.push(staffList[i]);
     }
   }
   renderStaffList(res);
 }
 
-document.querySelector('#searchName').onclick = searchStaff;
-document.querySelector('#searchName').oninput = searchStaff;
+document.querySelector('#searchName').onclick = find;
+document.querySelector('#searchName').oninput = find;
 
 
 //validation
@@ -178,7 +183,7 @@ var valid = true;
 valid = kiemTraRong(Staff.tenNV,'#name','Tên Nhân Viên');
 
 
-  
+
 
 
 
@@ -191,4 +196,38 @@ window.onload = function () {
     staffList = [];
   };
   renderStaffList(staffList);
+}
+
+
+
+function removeVietnameseTones(str) {
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/đ/g, "d");
+  str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+  str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+  str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+  str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+  str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+  str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+  str = str.replace(/Đ/g, "D");
+  // Some system encode vietnamese combining accent as individual utf-8 characters
+  // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+  str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+  str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+  // Remove extra spaces
+  // Bỏ các khoảng trắng liền nhau
+  str = str.replace(/ + /g, " ");
+  str = str.trim();
+  // Remove punctuations
+  // Bỏ dấu câu, kí tự đặc biệt
+  str = str.replace(
+    /!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g,
+    " "
+  );
+  return str;
 }
